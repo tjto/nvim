31047557252d8c2b134fbf7b7424c44a5ce1b6c9
tung.bui <tung.buiduy.91@gmail.com>
2021-11-27 22:28:43 +0200

    Require nvim tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -105,7 +105,10 @@ use {
 
 use {
     'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+        require('nvim-tree').setup {}
+    end
 }
 
 use { 'williamboman/nvim-lsp-installer'}
