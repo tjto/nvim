8d571d6b33cc50997f4be9c15cf42d2c3054fa17
tung.bui <tung.buiduy.91@gmail.com>
2022-01-25 00:37:57 +0200

    Add back tab/s-tab and autocomplete from 2 kw

--- lua/config/complete.lua
+++ lua/config/complete.lua
@@ -30,6 +30,27 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     },
 
     sources = {
@@ -56,7 +77,7 @@ cmp.setup {
         end,
     },
     completion = {
-        autocomplete = false
+        keyword_length = 2,
     },
 
 }
